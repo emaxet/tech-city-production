a9de60d5258dac79332a73b71fafa135a86917f1
Erik Tammpere <erik@Eriks-MacBook-Air.local>
2018-01-10 19:06:38 -0800

    a

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
     client: 'pg',
     debug: true,
-    connection: DB_URL,
+    connection: postgres://esglnrafgeryxp:a489ea2cc0020af18128336f1ad5fba8ee4cd3af6d18eace835ecf3355c6ee40@ec2-54-227-250-33.compute-1.amazonaws.com:5432/d3k27p2697l6tg,
     migrations: {
         tableName: 'migrations'
     },
